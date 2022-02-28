a32f52c5888246f30d1e5d734d0ca2e6c8ef0fcb
Павел Мышкин <pavelmyskin@MacBook-Pro-Pavel.local>
2022-02-28 15:47:52 +0500

    Добавлены urls и view-функции

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'posts.PostsConfig',
+    'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
