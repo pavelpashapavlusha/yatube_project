249f9e01730fb8457fb6b3b6d21acb97af0beb5f
Павел Мышкин <pavelmyskin@MacBook-Pro-Pavel.local>
2022-03-06 19:43:15 +0500

    Dz

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -5,8 +5,8 @@ app_name = 'users'
 
 urlpatterns = [
     path(
-      'logout/',
-      LogoutView.as_view(template_name='users/logged_out.html'),
-      name='logout'
+        'logout/',
+        LogoutView.as_view(template_name='users/logged_out.html'),
+        name='logout'
     ),
 ]
