638d7860e20e9d683e7d5e08013f9e900016bfb7
Павел Мышкин <pavelmyskin@MacBook-Pro-Pavel.local>
2022-03-13 21:25:53 +0500

    спринт 4

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
