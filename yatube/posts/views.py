d1838243a8856af7c64683b1b39564970c10bbcd
Павел Мышкин <pavelmyskin@MacBook-Pro-Pavel.local>
2022-03-13 21:23:32 +0500

    спринт 4

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-
-from django.shortcuts import render, get_object_or_404
-
-from .models import Post, Group
+from django.shortcuts import get_object_or_404, redirect, render
+from .forms import PostForm
+from .models import Group, Post
 
 LONG = 10
 
@@ -59,3 +60,31 @@ def post_detail(request, post_id):
         'count': count,
     }
     return render(request, 'posts/post_detail.html', context)
+
+
+@login_required
+def post_create(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect("posts:profile", request.user.username)
+    return render(request, 'posts/create_post.html', {'form': form})
+
+
+@login_required
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.user != post.author:
+        return redirect('posts:index')
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('posts:post_detail', post_id)
+    context = {
+        'form': form,
+        'post': post,
+        'is_edit': True,
+    }
+    return render(request, 'posts/create_post.html', context)
