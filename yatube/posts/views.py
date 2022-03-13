70e0e3f2147981d2e0c975660847712e4bd2a8ba
Павел Мышкин <pavelmyskin@MacBook-Pro-Pavel.local>
2022-03-13 21:50:44 +0500

    спринт 4

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -8,7 +8,7 @@ LONG = 10
 
 
 def index(request):
-    post_list = Post.objects.all().order_by('-pub_date')
+    post_list = Post.objects.all()
     paginator = Paginator(post_list, LONG)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -20,7 +20,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    post_list = Group.objects.all().order_by('-pub_date')
+    post_list = group.posts.all()
     paginator = Paginator(post_list, LONG)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
