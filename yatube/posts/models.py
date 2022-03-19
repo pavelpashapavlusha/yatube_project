e7c42adb9a57f434f1ee5d7beed97a395be6e409
Павел Мышкин <pavelmyskin@MacBook-Pro-Pavel.local>
2022-03-19 12:51:30 +0500

    sprint 4

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
 
@@ -7,7 +7,7 @@ User = get_user_model()
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
-    description = models.TextField(max_length=400)
+    description = models.TextField
 
     def __str__(self):
         return self.title
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
+
 from .forms import PostForm
 from .models import Group, Post, User
 
@@ -47,14 +48,12 @@ def profile(request, username):
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
-    group = post.group
-    author = post.author
-    count = Post.objects.filter(author=author).count()
+    post_list = post.posts.all()
     context = {
         'post': post,
-        'group': group,
-        'author': author,
-        'count': count,
+        'group': post.group,
+        'author': post.author,
+        'post_list': post_list,
     }
     return render(request, 'posts/post_detail.html', context)
 
