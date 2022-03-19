36c821b195a0a2a97ce3254232c3ca4b15a31ac4
Павел Мышкин <pavelmyskin@MacBook-Pro-Pavel.local>
2022-03-19 12:56:04 +0500

    sprint 4

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
-    description = models.TextField
+    description = models.TextField()
 
     def __str__(self):
         return self.title
